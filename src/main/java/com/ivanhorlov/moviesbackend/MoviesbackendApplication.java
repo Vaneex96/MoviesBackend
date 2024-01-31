241242d8e804963707964e769a464b1dd85a15b5
ivan <igorlov18@gmail.com>
2024-01-31 17:08:17 +0100

    first commit

---
+++ src/main/java/com/ivanhorlov/moviesbackend/MoviesbackendApplication.java
@@ -0,0 +1,15 @@
+package com.ivanhorlov.moviesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesbackendApplication.class, args);
+
+        System.out.println("moviesbackend");
+    }
+
+}
---
+++ src/test/java/com/ivanhorlov/moviesbackend/MoviesbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanhorlov.moviesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
